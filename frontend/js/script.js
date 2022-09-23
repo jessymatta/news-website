ff8d2f4ce682a574176b7deb49503acd0ec7e89a
jessymatta <jessy.w.matta@gmail.com>
2022-09-23 14:26:49 +0300

    frontend: added a get request using jQuery to get the data from the database and display it on the page

---
+++ frontend/js/script.js
@@ -0,0 +1,52 @@
+console.log("working??")
+const news_container = document.getElementById("news-containerr");
+
+// {
+//     "id": 1,
+//     "title": "Titlee 1",
+//     "picture": "pic url",
+//     "description": "aaaaaadddddddddd",
+//     "created_at": "2022-09-23 11:54:11"
+// }
+
+
+// Creating a news card and adding it to the page
+const addNews = (news) => {
+
+    let news_card_html = `            
+    <div class="col-12 col-md-6 col-lg-4">
+    <div class="card">
+        <img src="./images/${news.picture}">
+        <div class="card-body">
+            <h5 class="card-title">${news.title}</h5>
+            <p class="card-text">${news.description}</p>
+            <p> Posted at: ${news.created_at}</p>
+        </div>
+    </div>
+</div>`;
+    news_container.innerHTML += news_card_html;
+
+};
+
+//Getting data from get_news.php api using jQuery.get()
+$.get("http://localhost/news-website/backend/get_news.php")
+    .done((data) => {
+        console.log(data);
+        const all_news = [];
+        console.log("this is all news " + all_news);
+        console.log(typeof (data));
+        const data_json = JSON.parse(data);
+        console.log(data_json);
+
+
+        for (let news of data_json) {
+            console.log(news);
+            all_news.push(news);
+            addNews(news);
+        }
+
+    })
+    .fail((error) => console.error(error))
+    .always(() => console.log('Done'));
+
+
