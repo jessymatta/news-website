06d449f39f6ff50ef1daadba0c69bf804028f2d6
jessymatta <jessy.w.matta@gmail.com>
2022-09-23 11:28:54 +0300

    backend: added the sql file for the newsdb from which we will be getting our news data

---
+++ backend/news.sql
@@ -0,0 +1,26 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema newsdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `newsdb` DEFAULT CHARACTER SET utf8 ;
+USE `newsdb` ;
+
+-- -----------------------------------------------------
+-- Table `newsdb`.`news`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `newsdb`.`news` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `picture` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
